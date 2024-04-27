78c0686983420d736d5c65923fc5e5c6ff13dfe6
val antonini <vantonini@github.com>
2024-04-27 18:34:00 +1000

    switching over to Vec2

--- astar.go
+++ astar.go
@@ -18,20 +18,24 @@ func (p Pathfinder) Find(x1, y1, x2, y2 int) []Vec2 {
 	open := NewMinHeap(p.grid.Width, p.grid.Height)
 	closed := NewMaxGrid(p.grid.Width, p.grid.Height)
 
-	start := Node{Pos: Vec2{x1, y1}, F: 0, Weight: p.grid.Get(Vec2{x1, y1})}
+	start := Node{
+		Pos:    Vec2{x1, y1},
+		F:      0,
+		Weight: p.grid.Get(Vec2{x1, y1}),
+	}
 	open.Push(start)
 
 	for open.Len() > 0 {
 		q := open.Pop()
-		for _, successorVec2 := range getSuccessors(q.Pos.X, q.Pos.Y, p.grid.Width, p.grid.Height) {
+		for _, succ := range getSuccessors(q.Pos.X, q.Pos.Y, p.grid.Width, p.grid.Height) {
 			// cell is not open
-			if p.grid.Get(successorVec2) != 0 {
+			if p.grid.Get(succ) != 0 {
 				continue
 			}
 
 			successor := Node{
-				Pos:    Vec2{successorVec2.X, successorVec2.Y},
-				Weight: p.grid.Get(successorVec2),
+				Pos:    Vec2{succ.X, succ.Y},
+				Weight: p.grid.Get(succ),
 				Parent: &q,
 			}
 
@@ -47,8 +51,8 @@ func (p Pathfinder) Find(x1, y1, x2, y2 int) []Vec2 {
 				return path
 			}
 
-			successor.G = q.G + manhattan(q.Pos.X, q.Pos.Y, successor.Pos.X, successor.Pos.Y)
-			successor.H = manhattan(successor.Pos.X, successor.Pos.Y, x2, y2)
+			successor.G = q.G + manhattan(q.Pos, successor.Pos)
+			successor.H = manhattan(successor.Pos, Vec2{x2, y2})
 			successor.F = successor.G + successor.H
 			successor.Weight = p.grid.Get(successor.Pos)
 
@@ -64,8 +68,8 @@ func (p Pathfinder) Find(x1, y1, x2, y2 int) []Vec2 {
 	return []Vec2{}
 }
 
-func manhattan(x1, y1, x2, y2 int) int {
-	return abs(x1-x2) + abs(y1-y2)
+func manhattan(v1, v2 Vec2) int {
+	return abs(v1.X-v2.X) + abs(v1.Y-v2.Y)
 }
 
 func abs(x int) int {
