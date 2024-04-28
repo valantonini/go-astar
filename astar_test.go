4967373336a1d8d067699fedb1a2e27ea3c76c78
val antonini <vantonini@github.com>
2024-04-28 19:52:50 +1000

    work on diagonals

--- astar_test.go
+++ astar_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestGetSuccessors(t *testing.T) {
+func TestGetSuccessors_Cardinal(t *testing.T) {
 	w := []int{
 		1, 2, 3,
 		4, 5, 6,
@@ -81,6 +81,35 @@ func TestGetSuccessors(t *testing.T) {
 				}
 			}
 		})
+	}
+}
+
+func TestGetSuccessors_Diagonal(t *testing.T) {
+	w := []int{
+		1, 2, 3,
+		4, 5, 6,
+		7, 8, 9,
+	}
+	grid := NewGridFromSlice(3, 3, w)
+	got := getSuccessors(Vec2{1, 1}, grid.Width, grid.Height, diagonalSuccessors)
+
+	want := []Vec2{
+		{1, 0},
+		{2, 0},
+		{2, 1},
+		{2, 2},
+		{1, 2},
+		{0, 2},
+		{0, 1},
+		{0, 0},
+	}
+	if len(got) != len(want) {
+		t.Fatalf("len want %d got %d", len(want), len(got))
+	}
+	for i := range want {
+		if !reflect.DeepEqual(got[i], want[i]) {
+			t.Errorf("pos %d want %v got %v", i, want[i], got[i])
+		}
 	}
 }
 
